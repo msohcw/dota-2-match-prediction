0e6c6956d25156c02e7d0d165a32f64b1de22a18
Matthew Soh <matthewsoh.cw@gmail.com>
2016-10-26 00:56:54 -0700

    Model fits test data with 92% acc

--- learner.py
+++ learner.py
@@ -1,1 +1,79 @@
+from xgboost import XGBClassifier
+import numpy as np
+import pandas as pd
+from sklearn.cross_validation import train_test_split
+from sklearn.metrics import accuracy_score
+from sklearn.grid_search import GridSearchCV
+from sklearn.cross_validation import StratifiedKFold
 
+dataset = pd.read_csv('MatchOverviewTraining.csv')
+details = pd.read_csv('MatchDetail.csv')
+X = dataset.ix[: , :11]
+Y = dataset.ix[: , 11:]
+det = np.array(details)
+print("Done loading.", len(det))
+players = {}
+for row in det:
+  key = int(row[0]);
+  if key in players:
+    players[key].update({row[1]:row[2:]})
+  else:
+    players.update({key:{row[1]:row[2:]}})
+
+clean = []
+index = 0
+for row in np.array(X):
+    insert = []
+    key = int(row[0])
+    if key not in players:
+        player = {}
+    else:
+        player = players[key]
+    # print(match)
+    for i in range(1, 11):
+        replace = [0] * 21;
+        if row[i] in player: replace = list(player[row[i]])
+        if len(replace) < 21: 
+            replace.extend([0] * (21 - len(replace)))
+        replace.insert(row[i], 0)
+        insert.extend(replace)
+    if len(insert) != 220: print(len(insert))
+    clean.append(insert)
+# split data into train and test sets
+seed = 7
+test_size = 0.33
+X = np.array(clean)
+ 
+
+X_train, X_test, y_train, y_test = train_test_split(X, Y, test_size=test_size, random_state=seed)
+# fit model no training data
+model = XGBClassifier()
+y_train = (np.array(y_train)).flatten()
+model.fit(X_train, y_train)
+# make predictions for test data
+y_pred = model.predict(X_test)
+predictions = [round(value) for value in y_pred]
+# evaluate predictions
+accuracy = accuracy_score(y_test, predictions)
+print("Accuracy: %.2f%%" % (accuracy * 100.0))
+"""
+Y = np.array(Y).flatten()
+
+model = XGBClassifier()
+
+learning_rate = [0.0001, 0.001, 0.01, 0.1, 0.2, 0.3]
+param_grid = dict(learning_rate=learning_rate)
+kfold = StratifiedKFold(Y, n_folds=10, shuffle=True, random_state=7)
+grid_search = GridSearchCV(model, param_grid, scoring="neg_log_loss", n_jobs=-1, cv=kfold)
+
+result = grid_search.fit(X, Y)
+
+# summarize results
+print("Best: %f using %s" % (result.best_score_, result.best_params_))
+means, stdevs = [], []
+for params, mean_score, scores in result.grid_scores_:
+    stdev = scores.std()
+    means.append(mean_score)
+    stdevs.append(stdev)
+    print("%f (%f) with: %r" % (mean_score, stdev, params))
+"""
